bd026e0b9e6fc651c2d10376e9ffbc6b753c64c9
AllanContreras <allan.contreras-r@mail.escuelaing.edu.co>
2025-02-11 21:15:12 -0500

    Update Book.java

--- src/main/java/edu/eci/cvds/tdd/library/book/Book.java
+++ src/main/java/edu/eci/cvds/tdd/library/book/Book.java
@@ -1,30 +1,40 @@
 package edu.eci.cvds.tdd.library.book;
 
+import java.util.Objects;
+
 public class Book {
-    private final String tittle;
-    private final String author;
-    private final String isbn;
+    private String isbn;
+    private String title;
+    private String author;
 
-    public Book(String tittle, String author, String isbn) {
-        this.tittle = tittle;
-        this.author = author;
+    public Book(String isbn, String title, String author) {
         this.isbn = isbn;
-    }
-
-    public String getTittle() {
-        return tittle;
-    }
-
-    public String getAuthor() {
-        return author;
+        this.title = title;
+        this.author = author;
     }
 
     public String getIsbn() {
         return isbn;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     @Override
     public boolean equals(Object obj) {
-        return isbn.equals(((Book)obj).isbn);
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return Objects.equals(isbn, book.isbn);  // Solo comparamos por ISBN
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);  // Hash basado solo en ISBN
     }
 }
